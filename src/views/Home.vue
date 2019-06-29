200a529c16f4e9c46d9c636282cf4beed5f44703
linan <18321783268@163.com>
2019-06-29 21:32:38 +0800

    主页框架已搭好

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,124 @@
 <template>
-  <h1>这是首页！</h1>
+  <el-container>
+    <el-header>
+      <el-row type="flex" class="row-bg" justify="space-between" align="middle">
+        <el-col :span="6">
+          <img src="../assets/header_logo.png" width="200px" />
+        </el-col>
+        <el-col class="title">
+          <div>自由买后台管理系统</div>
+        </el-col>
+        <el-col :span="6">
+          <div class="login">
+            欢迎星耀会员墨迹
+            <a href="#">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          :default-active="$route.path"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :unique-opened="true"
+          :router="true"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="/user">
+              <i class="el-icon-menu"></i>
+              <span>用户管理</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<style>
+.el-container {
+  height: 100%;
+}
+.el-container .el-header {
+  padding: 0;
+  margin: 0;
+}
+.title {
+  text-align: center;
+  color: #fff;
+  font-size: 28px;
+  font-weight: 700;
+}
+.login {
+  color: #fff;
+}
+.login a {
+  color: chocolate;
+  text-decoration: none;
+  font-weight: bold;
+}
+.el-menu-vertical-demo.el-menu {
+  height: 100%;
+}
+</style>
