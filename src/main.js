b94d9d439cd676fcb0599a398a9ae55d3434e07b
linan <18321783268@163.com>
2019-07-02 23:36:12 +0800

    用户管理界面完成

--- src/main.js
+++ src/main.js
@@ -4,8 +4,28 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import "./assets/css/index.css";
+import axios from 'axios'
 
 Vue.use(ElementUI)
+
+// 通过defaults给axios设置一个默认的baseURL，可以在所有请求中都能用到这个地址
+axios.defaults.baseURL = "http://localhost:8888/api/private/v1/"
+
+// 设置axios的请求拦截器
+// axios.interceptors.request.use(config => {
+//   // config 就是拦截到的请求相关的所有的信息
+//   // 这个信息是可以进行修改的
+//   // console.log(config)
+//   config.headers.Authorization = localStorage.getItem("token")
+//   // return config不能动，这个函数中必须有这个内容
+//   return config
+// })
+
+// 设置axios的请求拦截器 方法二
+axios.defaults.headers.common['Authorization'] = localStorage.getItem("token")
+
+// 把axios加到Vue的原型上
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
